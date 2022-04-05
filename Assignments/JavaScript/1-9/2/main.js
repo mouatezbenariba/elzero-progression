886a4102c32ab545adc154e95987bfba3808972a
Elmouatez Billah Benariba <78697168+mouatezbenariba@users.noreply.github.com>
2022-04-05 17:43:15 +0100

    JS [01]-[09] [02] Assignments (Elzero) JS file

---
+++ Assignments/JavaScript/1-9/2/main.js
@@ -0,0 +1,10 @@
+// First method
+document.write(
+  '<h1 style="color:blue; font-size: 80px; font-weight: bold; text-align: center; font-family:arial">Elzero</h1>'
+);
+
+/*
+Second method
+look at the index.html file
+*/
+document.write('<h1 class="myClass">Elzero</h1>');
