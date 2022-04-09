9881e51a63845b7d75a99e82a2994c5bb772b76c
Elmouatez Billah Benariba <78697168+mouatezbenariba@users.noreply.github.com>
2022-04-09 13:43:31 +0100

    JS [18]-[22] [02] Assignments JS

---
+++ Assignments/JavaScript/18-22/2/main.js
@@ -0,0 +1,22 @@
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num * (true + true)); // 6
+
+// Solution Three
+console.log(num * num - num); // 6 
+
+// Soultion Four
+console.log(num++ + num - true ); // 6
+
+// Soultion Five
+console.log(num + --num - true); // 6
+
+// Solution Six
+console.log(--num ** num + num); // 6
+
+// Solution Seven
+console.log((--num + num + true) / (num / (num + num))); // 6
