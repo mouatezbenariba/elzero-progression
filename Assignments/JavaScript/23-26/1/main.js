381d4f477a6f2f589b4866ba24298c81cefd715a
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-04-12 14:25:31 +0100

    JS [23]-[26] [01] Assignments css

---
+++ Assignments/JavaScript/23-26/1/main.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(1e5); // 100000
+console.log(1e4 * 1e1); // 100000
+console.log(1e3 * 1e2); // 100000
+console.log(50000 + 50000); // 100000
+console.log(25000 * 4); // 100000
+console.log(2.5e4 * 4); // 100000
+console.log(1_00000); // 100000
+console.log(10_0000); // 100000
+console.log(1000_00); // 100000
+console.log(10000_0); // 100000
