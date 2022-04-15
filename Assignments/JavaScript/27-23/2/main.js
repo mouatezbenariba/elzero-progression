3190cfd847dcc21e6206d60ee4b7cae717169021
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-04-16 00:30:11 +0100

    JS [27]-[30] [02] Assignments JS file

---
+++ Assignments/JavaScript/27-23/2/main.js
@@ -0,0 +1,8 @@
+let word = 'Elzero';
+let letterZ = 'z';
+let letterE = 'e';
+let letterO = 'O';
+
+console.log(word.includes(letterZ)); // True
+console.log(word.startsWith(letterE.toUpperCase())); // True
+console.log(word.endsWith(letterO.toLowerCase())); // True
