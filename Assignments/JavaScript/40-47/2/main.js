e71746fac1568171df446028930318fa062df74e
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-06-19 15:35:31 +0100

    JS [40]-[47] [02] js file

---
+++ Assignments/JavaScript/40-47/2/main.js
@@ -0,0 +1,8 @@
+let friends = ['Ahmed', 'Eman', 'Osama', 'Gamal'];
+
+// Write Your Code Here
+
+let first = friends.shift();
+let last = friends.pop();
+
+console.log(friends); // ["Eman", "Osama"]
