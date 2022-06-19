9ccb3630532a0fd1b552ac9ea1cc35dcd6d06bd4
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-06-19 16:01:19 +0100

    JS [40]-[47] [06] js file

---
+++ Assignments/JavaScript/40-47/6/main.js
@@ -0,0 +1,7 @@
+let arr1 = ['A', 'C', 'X'];
+let arr2 = ['D', 'E', 'F', 'Y'];
+let allArrs = [];
+
+allArrs = arr1.concat(arr2).sort().slice(-arr1.length).join('').toLowerCase();
+
+console.log(allArrs); // fxy
