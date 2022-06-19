a57c6d08d7f654d91f56e3e7549700e70f099baf
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-06-19 15:37:01 +0100

    JS [40]-[47] [03] js file

---
+++ Assignments/JavaScript/40-47/3/main.js
@@ -0,0 +1,7 @@
+let arrOne = ['C', 'D', 'X'];
+let arrTwo = ['A', 'B', 'Z'];
+let finalArr = arrOne.concat(arrTwo).sort().reverse();
+
+// Write One Single Line Of Code
+
+console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
