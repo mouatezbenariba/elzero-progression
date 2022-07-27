bff52e3cc1462b2e75f599b90e967974a36c580a
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-07-27 12:20:10 +0100

    add assignment 86-93-1 js file

---
+++ Assignments/JavaScript/86 - 93/1/main.js
@@ -0,0 +1,18 @@
+let myElement = document.getElementById('elzero');
+myElement = document.getElementsByTagName('div');
+myElement = document.getElementsByName('js');
+myElement = document.getElementsByClassName('element');
+myElement = document.querySelector('#elzero');
+myElement = document.querySelector('.class');
+myElement = document.querySelector('[name = "js"]');
+myElement = document.querySelector('div');
+myElement = document.querySelectorAll('div')[0];
+myElement = document.querySelectorAll('.element')[0];
+myElement = document.querySelectorAll('#elzero')[0];
+myElement = document.querySelectorAll('[name = "js"]')[0];
+myElement = document.body.children[0];
+myElement = document.body.children[0];
+myElement = document.body.firstElementChild;
+myElement = document.body.childNodes[1];
+
+console.log(myElement);
