c2e2517676c1c3267bbe8d843f97d3177ee05df4
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-07-27 12:32:58 +0100

    add assignment 68-93-2

---
+++ Assignments/JavaScript/86 - 93/2/main.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 10; i++) {
+  let myElement = document.images[i];
+  myElement.setAttribute(
+    'src',
+    'https://elzero.org/wp-content/themes/elzero/imgs/logo.png'
+  );
+  myElement.setAttribute('alt', 'Elzero Logo');
+}
