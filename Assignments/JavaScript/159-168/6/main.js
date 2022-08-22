7fbb67f7ba7508686f569d9b80651c93ad131820
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-08-22 22:10:47 +0100

    add assignment 159-168-06 js file

---
+++ Assignments/JavaScript/159-168/6/main.js
@@ -0,0 +1,21 @@
+// Write Your Generator Function Here
+function* gen() {
+  let i = 14;
+  let n = 0;
+  while (true) {
+    yield i;
+    i += 140 + 200 * n++;
+  }
+}
+
+let generator = gen();
+
+console.log(generator.next()); // {value: 14, done: false}
+console.log(generator.next()); // {value: 154, done: false}
+console.log(generator.next()); // {value: 494, done: false}
+console.log(generator.next()); // {value: 1034, done: false}
+console.log(generator.next()); // {value: 1774, done: false}
+console.log(generator.next()); // {value: 2714, done: false}
+console.log(generator.next()); // {value: 3854, done: false}
+console.log(generator.next()); // {value: 5194, done: false}
+console.log(generator.next()); // {value: 6734, done: false}
