7a8a512169023b03be6b925ea80b2901cfc4d88a
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-08-22 22:14:08 +0100

    add assignment 159-168-08 js mod one file

---
+++ Assignments/JavaScript/159-168/8/mod-two.js
@@ -0,0 +1,11 @@
+export { modOne };
+
+let a = 10; // Do Not Edit Names
+let b = 20; // Do Not Edit Names
+let c = 30; // Do Not Edit Names
+
+let modOne = new Object();
+
+modOne.numOne = a;
+modOne.numTwo = b;
+modOne.numThree = c;
