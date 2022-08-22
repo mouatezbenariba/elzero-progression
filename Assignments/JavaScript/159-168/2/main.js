787edd18d50df0053f23ad7c6bb1954b581da4ed
Elmouatez Billah Benariba <benariba.mouatez@gmail.com>
2022-08-22 22:03:01 +0100

    ad assignment 159-168-02 js file

---
+++ Assignments/JavaScript/159-168/2/main.js
@@ -0,0 +1,7 @@
+let date = new Date(0);
+date.setSeconds(1);
+date.setHours(0);
+console.log(date);
+// 'Tue Jan 01 1980 00:00:01 GMT+0200 (Eastern European Standard Time)';
+
+// The result may not appear accurate in the codepen console, so try to run it in your localhost, thank you!
