713f7d57d6df12e3d16f1b66c111ace068baa9ff
mouatezbenariba <benariba.mouatez@gmail.com>
2026-05-09 16:13:50 +0100

    add assignment PHP : 1-5:1

---
+++ Assignments/PHP/1-5/1/index.php
@@ -0,0 +1,20 @@
+<!--
+Hello my name is Mouatez
+My age is 29
+I live in Algeria
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset=echo"UTF-8";>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "My First PHP Page" ?></title>
+  </head>
+  <body>
+    <div><?php echo "We Love PHP"; ?></div>
+    <div><?php print "Elzero Channel"; ?></div>
+  </body>
+</html>
