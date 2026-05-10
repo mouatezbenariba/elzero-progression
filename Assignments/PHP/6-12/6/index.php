7385047d02b756a06de71f60c8353e0b975319b2
mouatezbenariba <benariba.mouatez@gmail.com>
2026-05-10 19:26:48 +0100

    add assignment PHP : 6-12:6

---
+++ Assignments/PHP/6-12/6/index.php
@@ -0,0 +1,13 @@
+<?php
+$something = "Programming";
+
+echo <<<code
+Hello \PHP\
+We Love $something
+code;
+
+// [1] Fix The Error
+// [2] Remove 2 Characters To Get The Output
+
+// Needed Output
+// Hello \PHP\ We Love Programming
