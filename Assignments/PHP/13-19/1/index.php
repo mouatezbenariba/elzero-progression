534bb102debaa5a53b2cf096f9a00d7f909b9a84
mouatezbenariba <benariba.mouatez@gmail.com>
2026-05-11 00:15:47 +0100

    add assignment PHP : 13-19:1

---
+++ Assignments/PHP/13-19/1/index.php
@@ -0,0 +1,26 @@
+<?php $coursename= "Elzero courses" ?>
+
+<?php 
+
+echo "</br>";
+echo "$coursename";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php echo "$coursename"; ?>">
+    <title>Welcome To <?php echo "$coursename"; ?></title>
+  </head>
+  <body>
+    <h1><?php echo "$coursename"; ?></h1>
+    <p>Here In <?php echo "$coursename"; ?> We Provide Front-End And Back-End Courses</p>
+    <hr>
+    <div><?php echo "$coursename"; ?> Is The What You Need.</div>
+    <footer>All Right Reserved To <?php echo "$coursename"; ?></footer>
+  </body>
+</html>
