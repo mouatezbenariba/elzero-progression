0d6681ee5feadcf50bd16f50c346045957d0c3ca
mouatezbenariba <benariba.mouatez@gmail.com>
2026-05-10 19:26:41 +0100

    add assignment PHP : 6-12:5

---
+++ Assignments/PHP/6-12/5/index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<pre>";
+echo <<<'testFive'
+    Hello "'Elzero'"
+    We Love $Programming$
+    Languages Specially "PHP"
+testFive;
+echo "</pre>";
+
+// Needed Output
+/*
+
+Hello "'Elzero'"
+We Love $Programming$
+Languages Specially "PHP"
+
+*/
