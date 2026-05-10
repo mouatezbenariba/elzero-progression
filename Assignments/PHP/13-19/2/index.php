cfdd8e451a4f65e3a8f72df44994fc6243d9e79b
mouatezbenariba <benariba.mouatez@gmail.com>
2026-05-11 00:28:47 +0100

    add assignment PHP : 13-19:2

---
+++ Assignments/PHP/13-19/2/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+$name = "elzero";
+$$name = "Web";
+
+echo "${$name}";
+echo '<br>';
+echo $${'name'};
+echo '<br>';
+echo $$name;
+echo '<br>';
+echo "$elzero";
+echo '<br>';
+echo ${'elzero'};
