f6df68e60cb9e63d892dedc3797aee97973fc53c
mouatezbenariba <benariba.mouatez@gmail.com>
2026-05-10 18:31:37 +0100

    add assignment PHP : 6-12:2

---
+++ Assignments/PHP/6-12/2/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$number = 100;
+
+echo gettype($number);
+echo '<br>';
+var_dump($number);
+echo '<br>';
+echo get_debug_type($number);    
+
+// Method One
+// Method Two
+// Method Three => Optional100;
