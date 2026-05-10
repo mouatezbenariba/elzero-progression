4fdf9699584ea61d3d385449e3eea541fb7fc2c3
mouatezbenariba <benariba.mouatez@gmail.com>
2026-05-10 18:50:01 +0100

    add assignment PHP : 6-12:4

---
+++ Assignments/PHP/6-12/4/index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "<pre>";
+    echo <<<"newLine"
+    We
+    Love
+    Elzero
+    Web
+    School;
+    newLine;
+echo "</pre>";
+
+
+// Needed Output
+/*
+We
+Love
+Elzero
+Web
+School
+*/
