933038833b9dda4543854e9365582fd1ea413138
mouatezbenariba <benariba.mouatez@gmail.com>
2026-05-11 01:18:53 +0100

    add assignment PHP : 13-19:4

---
+++ Assignments/PHP/13-19/4/index.php
@@ -0,0 +1,17 @@
+<?php
+
+phpinfo();
+
+echo $_SERVER['DOCUMENT_ROOT'] . "<br>";
+echo $_SERVER['SERVER_NAME'] . "<br>";
+// echo $_SERVER['SystemRoot'] . "<br>"; Windows Only.
+// echo $_SERVER['OPENSSL_CONF'] . "<br>"; Didn't work on my system (UBUNTU)
+
+
+ /*
+"C:/xampp/htdocs"
+"localhost"
+"C:\WINDOWS"
+"C:/xampp/apache/bin/openssl.cnf"
+
+*/
