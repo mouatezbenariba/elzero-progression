bce70aa40cc0fcd242f4315f856c6a6054c9a690
mouatezbenariba <benariba.mouatez@gmail.com>
2026-05-11 01:32:53 +0100

    add assignment PHP : 13-19:5

---
+++ Assignments/PHP/13-19/5/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// define('xor', 'Value of Constant');
+// echo xor; best method to check for reserved keywords is to try to use them as constants, if it works then it is not a reserved keyword, if it gives an error then it is a reserved keyword
+
+
+/*
+  function
+  echo
+  break
+  class
+  print
+  array
+  if
+  for
+  while
+  and
+  or
+  xor
+*/
