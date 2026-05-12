ecb11eafd94b75f956e6f641fa9651c62631c7df
mouatezbenariba <benariba.mouatez@gmail.com>
2026-05-12 15:42:12 +0100

    add assignment PHP : 20-29:5

---
+++ Assignments/PHP/20-29/5/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+$points = 10;
+
+$points++; // 11
+$points++; // 11
+$points++; // 11
+
+echo $points; // 13
+
+$points--; // 11
+$points--; // 11
+$points--; // 11
+$points--; // 11
+$points--; // 11
+
+echo "<br>";
+echo $points; // 8;
