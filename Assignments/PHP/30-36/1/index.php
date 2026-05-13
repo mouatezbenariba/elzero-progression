2e242ca1b50067b02708ed90a8a2988d587b26c0
mouatezbenariba <benariba.mouatez@gmail.com>
2026-05-13 18:36:50 +0100

    add assignment PHP : 30-36:1

---
+++ Assignments/PHP/30-36/1/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+$a = 100;
+$b = 200;
+$c = 100;
+
+
+echo ($b > $a)? "Yes" : "No";
+echo "<br>";
+echo ($a === $c)? "Yes" : "No";
+echo "<br>";
+echo ($a + $c === $b)? "Yes" : "No";
+echo "<br>";
+
+
+/*
+  Check That:
+  Variable "b" Larger Than Variable "a"
+  Variable "a" Identical To Variable "c"
+  Variable "a" Plus Variable "c" Identical To Variable "b"
+*/
